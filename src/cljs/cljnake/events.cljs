73913b5eedc23995ef83f4829b6144d361d16601
Kiran Gangadharan <me@kirang.in>
2018-01-29 17:03:23 +0530

    Manage intervals using code snippet from re-frame doc
    - https://github.com/Day8/re-frame/blob/master/docs/FAQs/PollADatabaseEvery60.md#a-side-note-about-effect-handlers-and-figwheel

--- src/cljs/cljnake/events.cljs
+++ src/cljs/cljnake/events.cljs
@@ -8,10 +8,25 @@
  (fn  [_ _]
    db/default-db))
 
+(defonce interval-handler
+  (let [intervals (atom {})]
+    (fn handler [{:keys [action id event frequency]}]
+      (condp = action
+        :start (swap! intervals assoc id
+                      (js/setInterval
+                       #(rf/dispatch event)
+                       frequency))
+        :stop  (do (js/clearInterval (id @intervals))
+                   (swap! intervals dissoc id))
+        :clean (doseq [id (keys @intervals)]
+                 (handler {:action :stop :id id}))))))
+
+;; when this code is reloaded `:clean` existing intervals
+(interval-handler {:action :clean})
+
 (rf/reg-fx
- ::start-game
- (fn []
-   (js/setInterval #(rf/dispatch [::tick]) 150)))
+ ::tick
+ interval-handler)
 
 (rf/reg-event-db
  ::change-snake-direction
